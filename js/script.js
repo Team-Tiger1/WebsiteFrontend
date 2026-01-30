2aa00e5509ce62e2c37492644c4a937fa8a5d7e6
willfoulger1 <wjf208@exeter.ac.uk>
2026-01-30 16:55:22 +0000

    navbar

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
+const overlay = document.getElementById('nav-overlay');
 
 if (bar){
     bar.addEventListener('click', () => {
         nav.classList.add('active');
+        overlay.classList.add('active')
     })
 }
 
 if (close){
     close.addEventListener('click', () => {
         nav.classList.remove('active');
+        overlay.classList.remove('active');
     })
+}
+
+if (overlay) {
+  overlay.addEventListener('click', () => {
+    nav.classList.remove('active');
+    overlay.classList.remove('active');
+  });
 }
 
 const logoutBtn = document.getElementById("logoutBtn");
