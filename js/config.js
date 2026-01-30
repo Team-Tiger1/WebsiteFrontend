50eae1cbb48465b0352d40f4732c298033b50a88
Dan Jackson <danieljackson634@gmail.com>
2026-01-30 14:31:20 +0000

    Fixed endpoint url

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 //check if website is running locally
-const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+// const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+const isLocal = false;
 
 //export API url based on the above check for product or local deployment
 export const API_URL = isLocal
