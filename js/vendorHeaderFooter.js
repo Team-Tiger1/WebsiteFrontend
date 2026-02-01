81cd00ebfd63fce5df085da1701f2929819a9b66
Dan Jackson <danieljackson634@gmail.com>
2026-02-01 02:56:47 +0000

    Injecting Header and Footer into Vendor Pages

---
+++ js/vendorHeaderFooter.js
@@ -0,0 +1,107 @@
+function addHeader() {
+    return `<header>
+    <nav class="navbar">
+
+
+      <ul id="navbar">
+
+        <li>
+          <a href="catalog.html" class="navbar-logo">
+            <img src="img/simp_logo.png" alt="">
+          </a>
+        </li>
+
+        <li><a href="dashboard.html">Dashboard</a></li>
+        <li><a href="newproduct.html">Create Products</a></li>
+        <li><a href="newbundle.html">Create Bundle</a></li>
+        <li><a href="manage.html">Manage Pickups</a></li>
+        <li><a href="analytics.html">Analytics</a></li>
+        <li><a href="forecast.html">Forecast</a></li>
+
+
+        <a href="#" id="close"><i class="fa fa-times" aria-hidden="true"></i></a>
+
+      </ul>
+
+      <div id="mobile">
+        <i id="bar" class="fa fa-outdent" aria-hidden="true"></i>
+        <a href="cart.html"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+      </div>
+    </nav>
+  </header>`;
+
+}
+
+function addFooter() {
+
+    return `<footer class="part1">
+    <div class="col">
+      <h4>Contact</h4>
+      <p> <strong>Address:</strong> Northcote House, The Queens's Drive, Exeter, Devon, EX4 4QJ, United Kingdom</p>
+      <p> <strong>Phone:</strong> +07766832743</p>
+      <p> <strong>Hours:</strong> 10:00-18:00, Mon - Sun</p>
+     
+    </div>
+
+    <div class="col">
+      <h4>About</h4>
+      <a href="#">About us</a>
+      <a href="#">Delivery Information</a>
+      <a href="#">Privacy Policy</a>
+      <a href="#">Terms and Conditions</a>
+      <a href="#">Contact Us</a>
+    </div>
+
+    <div class="col">
+      <h4>My Account</h4>
+      <a href="orders.html">View Orders</a>
+      <a href="#">Help</a>
+    </div>
+
+    <div class="copyright">
+      <p>© 2026, The Last Fork</p>
+    </div>
+  </footer>`;
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.getElementById("header");
+    const footer = document.getElementById("footer");
+
+    if(header != null) {
+        header.innerHTML = addHeader();
+    }
+
+    if(footer != null) {
+        footer.innerHTML = addFooter();
+    }
+
+    const bar = document.getElementById('bar');
+    const close = document.getElementById('close');
+    const nav = document.getElementById('navbar');
+    const overlay = document.getElementById('nav-overlay');
+
+    if (bar){
+        bar.addEventListener('click', () => {
+            nav.classList.add('active');
+            overlay.classList.add('active')
+        })
+    }
+
+    if (close){
+        close.addEventListener('click', () => {
+            nav.classList.remove('active');
+            overlay.classList.remove('active');
+        })
+    }
+
+    if (overlay) {
+        overlay.addEventListener('click', () => {
+            nav.classList.remove('active');
+            overlay.classList.remove('active');
+        });
+    }
+
+});
